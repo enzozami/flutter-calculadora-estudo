89c578b2095af5b12575a41d3842547308de653f
Enzo Zamineli <enzozamineli@gmail.com>
2025-09-09 15:25:41 -0300

    teste

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:calculadora/app/module/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.black,
+      home: HomePage(),
+    );
+  }
+}
